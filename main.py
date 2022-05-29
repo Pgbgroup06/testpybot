f5e8079d7c502e78fa6010a9efd54d77f9860174
Pgbgroup06 <103879971+Pgbgroup06@users.noreply.github.com>
2022-05-29 14:51:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-import time
-import json
-import telebot
+from pyrogram import Client,filters
+
+bot = Client(
+  "Pgbgroup Bot",
+  api_id=9914709,
+  api_hash ="b3e7cb5ef23aaa7eb618a63d62405e1d",
+  bot_token ="5313087885:AAE8HWHhfy-bT7vznBcpUFsIrrlw3eMBvpQ"
+)
+
+@bot.on_message(filters.command('start') & filters.private)
+def startmsg(bot,message):
+  bot.send_message(message.chat.id,"Hi I Am Online")
 
 
+@bot.on_message(filters.command('info') & filters.private)
+def infomsg(bot,message):
+  bot.send_message(message.chat.id,"Made By Pgbgroup")
 
-BOT_TOKEN = "5399300382:AAFTPLtLwPK8yfZynK_7ZG-S-SlHXAvKPj4"
+print("Running")
+bot.run()
 
-OWNER_ID = 1629550450 #write owner's user id here.. get it from @MissRose_Bot by /id
-
-
-bot = telebot.TeleBot(BOT_TOKEN)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-   try:
-    user = message.chat.id
-    msg = message.text
-    if msg == '/start':
-        user = str(user)
-        
-        bot.send_message(user, "Hi")
-   
-            
-   if __name__ == '__main__':
-    bot.polling(none_stop=True)
-         
  
         
        
